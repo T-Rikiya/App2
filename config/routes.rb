059758a1bde779a37374da2ab8f9c2ee9509ff1b
EC2 Default User <ec2-user@ip-172-31-6-61.ap-northeast-1.compute.internal>
2023-05-16 13:30:46 +0000

    16syo-end

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
     # get "homes/about" => "homes#about", as: "about" の短縮系
     get 'homes/about' , as: 'about' 
     resources :post_images, only: [:new, :create, :index, :show, :destroy]
-    resources :users, only: [:show, :edit]
+    resources :users, only: [:show, :edit, :update]
 end
